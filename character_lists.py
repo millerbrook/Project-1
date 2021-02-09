bf4b729bded1e27d8289bfca5c538f36c3dfc605
Brook Miller <millerbrook@gmail.com>
2021-02-09 16:31:23 -0500

    Working code for character calls and comic calls from Marvel API

---
+++ character_lists.py
@@ -0,0 +1,35 @@
+#Marvel Cinematic Universe Characters Information
+#*************************************************
+char_names = ['Spider-Man', 'Iron Man', 'Hulk', 'Captain America', 
+    'Black Widow', 'Hawkeye', 'Captain Marvel', 'Star-Lord', 'Thor',
+    'Nick Fury', 'War Machine', 'Winter Soldier', 'Falcon', 'Wanda Maximoff',
+    'Vision', 'Ant-Man', 'Wasp', 'Black Panther', 'Dr. Strange', 'Gamora', 'Groot',
+    'Rocket', 'Drax', 'Nebula', 'Mantis', 'Loki', 'Thanos']
+#from Wikipedia
+
+support_chars = ['Peggy Carter', 'Phil Coulson', 'Jane Foster', 'Heimdall', 'Maria Hill', 'Happy Hogan',
+    'J.A.R.V.I.S', 'Pietro Maximoff', 'Odin', 'Okoye', 'Pepper Potts', 'Hank Pym', 'Thunderbolt Ross', 
+    'Erik Selvig', 'Shuri', 'Howard Stark', 'Valkyrie', 'Janey van Dyne', 'Wong', 'Helmut Zemo',
+    'Laufey', 'Frigga', 'Fandral', 'Hogun', 'Volstagg', 'Destroyer', 'Crossbones', 'Red Skull', 
+    'Arnim Zola', 'Jacques Dernier', 'Dum Dum Dugan', 'James Montgomery Falsworth', 'Gabe Jones',
+    'Jim Morita', 'Ulysses Klaue', 'Ultron', 'Yondu Udonta', 'Aunt May', 'Ned Leeds', 'Flash Thompson', 
+    'MJ', 'Betty Brant', 'Roger Harrington', 'Mysterio', 'J. Jonah Jameson', 'Talos', 'Soren', 'Vulture', 
+    'Hela', 'Erik Killmonger', 'Korg', 'Shocker', 'Phineas Mason', 'Mac Gargan', 'Aaron Davis', 
+    'Ancient One', "M'Baku", 'Yon-Rogg', 'Supreme Intelligence', 'Ronan the Accuser', 'Korath', 'Bill Foster',
+    'Ghost', 'Jimmy Woo', 'Cassie Lang', 'Yellowjacket', 'Luis', 'Ego', 'Grandmaster', 'Collector',
+    'Skurge', 'Malekith', 'Kaecilius', 'Alexander Pierce', 'Agent 13', "T'Chaka", 'Jackson Norriss',
+    'The Mandarin', 'Nakia', 'Ramonda', 'Zuri', "N'Jobu", "Ebony Maw", "Corvus Glaive", "Proxima Midnight",
+    'Cull Obsidian']
+#from Wikipedia
+
+minor_chars = ['Abomination', 'Matthew Ellis', 'Rodriguez', 'Abraham Erskine', 'Christine Everhart', 'Justin Hammer'
+    'Darcy Lewis', 'List', 'Gideon Malick', 'Kraglin Obfonteri', 'The Other', 'Christine Palmer',
+    'Chester Phillips', 'Heinz Kruger', 'Raza', 'Betty Ross', 'Sif', 'Jasper Sitwell', 'Obadiah Stane',
+    'Stern', 'Samuel Sterns', 'Wolfgang von Strucker', 'Ho Yinsen', 'Anton Vanko', 'Whiplash', 'Aldrich Killian'
+    'Maya Hansen', 'Trevor Slattery', 'Helen Cho', 'Eitri', 'Howard the Duck', 'Rhomann Dey', 'Nova Prime',
+    'Everett K. Ross', 'Karl Mordo', 'Jonathan Pangborn', 'Surtur', 'Maria Rambeau', 'Monica Rambeau',
+    'Mar-Vell', 'Dormammu', 'F.R.I.D.A.Y', 'Harley Keener', 'Jim Paxton', 'Dave', 'Kurt', 'Miek', 'Minn-Erva',
+    'Att-Lass', 'Bron-Char', "W'Kabi", 'Agnes', 'Tyler Hayward', 'Stakar Ogord', 'Martinex', 'Charlie-27',
+    'Aleta Ogord', 'Krugarr', 'Mainframe', 'Taserface', 'Ayesha', 'Sonny Burch', 'William Ginter Riva',
+    'Watcher Informant']
+#from Wikipedia 
---
+++ config.py
@@ -0,0 +1,4 @@
+#MARVEL API KEY
+pub_key = '3cbd89298065cd6f05ed29c196f7c4d7'
+priv_key = '62814909be6dda12ee94bb8a81f93bd78c6f4601'
+hash_key = 'baff0f640a91ce1962dee0d176b32788'
